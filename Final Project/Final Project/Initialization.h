f2bfbd80f8e89d39e3a6c74a091e4aba8f582ced
Thomas Bolinger <bolitj01@students.ipfw.edu>
2017-11-15 21:27:18 -0500

    Changed mouse camera rotation and refactored code for final project development

---
+++ Final Project/Final Project/Initialization.h
@@ -0,0 +1,15 @@
+#ifndef INITIALIZATION_H
+#define INITIALIZATION_H
+
+#include "GlobalVariables.h"
+
+//Functions associated with initilization of the environment or data
+
+void initWindow(int &argc, char **argv);
+
+void initializeViewingVolume();
+
+
+
+#endif // !INITIALIZATION_H
+
--- Final Project/Final Project/Main.cpp
+++ Final Project/Final Project/Main.cpp
@@ -1,12 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _USE_MATH_DEFINES
-
 #include "DrawModels.h"
 #include "GlobalVariables.h"
-#include "Interaction.h"
+#include "CallbackFunctions.h"
+#include "Initialization.h"
+
+/*
+Thomas Bolinger
+Kimberley Grobien
+Soccer game final project for CS321 Intro to Computer Graphics
+Fall 2017
+*/
 
 FILE *fp;
-char fileName[50]; //Holds the  file name
+char fileName[50]; //Holds the file name
 int numOfLines; //Number of lines in the input file
 int numOfData; //Number of data in file
 int numOfV; //Number of vertices in file
@@ -23,61 +28,29 @@ int xMove = 0;//Holds amount to move along x axis
 int yMove = 0;//Holds amount to move along y axis
 int zMove = 0;//Holds amount to move along z axis
 double scaleAmount = 1.0;//Holds amount to scale by
-double origX, origY;//Holds the original x and y values when mouse is depressed
+double previousMouseX, previousMouseY;//Holds the original x and y values when mouse is depressed
 int mode = 0;//Defines mode for mouse motion. Holds 0 for drag and drop mode and 1 for rotate mode
-double *verticeArrG;
-double *normVecArrG;
-int *faceArrG;
-double *verticeArrB;
-double *normVecArrB;
-int *faceArrB;
-double *verticeArrS;
-double *normVecArrS;
-int *faceArrS;
+double *verticeArrGoal;
+double *normVecArrGoal;
+int *faceArrGoal;
+double *verticeArrBall;
+double *normVecArrBall;
+int *faceArrBall;
+double *verticeArrStadium;
+double *normVecArrStadium;
+int *faceArrStadium;
 int currObj = 0; //Object file being read, 0 = ball, 1 = goal
-
-
-void initWindow(int &argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Problem #1");
-}
-
-void display(void) {
-
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-200, 200, -200, 200, 200, 1000);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, 0, 800, 0, 0, 0, 0, 1, 0);
-	glRotatef(xRot, 1.0, 0.0, 0.0);
-	glRotatef(yRot, 0.0, 1.0, 0.0);
-	glRotatef(zRot, 0.0, 0.0, 1.0);
-	glScalef(scaleAmount, scaleAmount, scaleAmount);
-	glTranslatef(xMove, yMove, zMove);
-	drawFileB();
-	drawFileG();
-	//drawFileS()
-
-	glFlush();
-}
 
 void setup() {
 	//Reads data for models into arrays 
 	fp = fopen("soccerball.obj", "r");
 	countLines();
-	createArrayB();
+	createBallData(); 
 	currObj = 1;
 	
 	fp = fopen("soccergoal.obj", "r");
 	countLines();
-	createArrayG();
+	createGoalData();
 	currObj = 2;
 
 /*	fp = fopen("stadium.obj", "r");
@@ -89,9 +62,12 @@ void main(int argc, char** argv)
 {
 		setup();
 		initWindow(argc, argv);
+
+		initializeViewingVolume();
+
 		glutDisplayFunc(display);
-		glutMouseFunc(mouse);
-		glutMotionFunc(mouseMotion);
+		glutMouseFunc(mouseClick);
+		glutMotionFunc(mouseCameraRotation);
 		glutKeyboardFunc(keyPress);
 		glutMainLoop();
 
